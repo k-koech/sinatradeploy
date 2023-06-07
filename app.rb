41da661d783de71d1069c5e1b7aa95d799ed5df8
k-koech <koechkelvin97@gmail.com>
2023-06-07 07:04:51 +0300

    deploy

---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sqlite3'
+require 'sinatra/activerecord'
+
+# Set up the database connection
+set :database, { adapter: "sqlite3", database: "database.db" }
+class User < ActiveRecord::Base
+end
+
+# Create the 'users' table if it does not exist
+unless User.table_exists?
+    ActiveRecord::Schema.define do
+      create_table :users do |t|
+        t.string :name
+        t.timestamps null: false
+      end
+    end
+  end
+
+
+
+
+# Define your routes and application logic
+get '/' do
+    @users = User.all
+    erb :index
+  end
+  
+  post '/users' do
+    name = params[:name]
+    User.create(name: name)
+    redirect '/'
+  end
